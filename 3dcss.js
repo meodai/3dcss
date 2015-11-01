be5d030fb23cede54ac6e7eecc2c95f6e39252ee
David Aerne <meodai@gmail.com>
2015-11-01 21:59:04 +0100

    new NPM version

--- 3dcss.js
+++ 3dcss.js
@@ -23,11 +23,8 @@
     var pre;
     var dom;
     styles = window.getComputedStyle(document.documentElement, '');
-    pre = (Array.prototype.slice
-          .call(styles)
-          .join('')
-          .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
-        )[1];
+    pre = (Array.prototype.slice.call(styles).join('')
+          .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o']))[1];
     dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
     return {
       dom: dom,
